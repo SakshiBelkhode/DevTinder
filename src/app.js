db00456a7f602afd616291fbd92b06b2302af3ad
SakshiBelkhode <126399972+SakshiBelkhode@users.noreply.github.com>
2025-11-18 18:16:14 +0530

    Updated Todays Changes

--- src/app.js
+++ src/app.js
@@ -1,18 +1,30 @@
-const express = require("express");
+const express = require("express")
 
 const app = express();
 
-app.use("/", (req, res) => {
-    res.send("Namaste Sakshi!");
-}); 
+app.use("/user" , (req, res) => {
+    res.send("HAHAHAHAHAHA");
+});
 
-app.use("/hello", (req, res) => {
-    res.send("Hello hello hello!");
-}); 
+//This will only handle GET call to/ user
+app.get("/user", (req, res) => {
+    res.send({firstname: "Sakshi" , lastname: "Belkhode"});
+});
 
+app.post("/user" , (req, res)=> {
+    //saving data to DB
+    res.send("Data Successfully saved to database");
+});
+
+app.delete("/user", (req , res) => {
+    res.send("Deleted Sussessfully");
+});
+
+//This will match all the HTTP method API calls to /test
 app.use("/test", (req, res) => {
-    res.send("Hello from the server!");
+    res.send("Hello from the server");
 }); 
+
 
 app.listen(7777, () => {
     console.log("Server is successfully listening on port 7777.....")
